f6458e2fe9e50ebe87892e57a08decf6a54a53cc
Clemens Sietas <siecle@gmx.de>
2026-06-03 12:21:06 +0200

    Twig installed after composer and first template works

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        $contents = $this->render('home/index.html.twig');
+        return $contents;
+    }
+}
