1882dfbe8989f6ab50e2525ec6e6916910d86488
Clemens Sietas <siecle@gmx.de>
2026-06-04 22:41:23 +0200

    Get the ID from a request

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,19 +4,24 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 final class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index', methods: ['GET', 'HEAD'])]
     public function index(ProductRepository $repository): Response
     {
-        // $products = $repository->findAll();
-        // dd($products);
-
         return $this->render('product/index.html.twig', [
             'products' => $repository->findAll()
         ]);
+    }
+
+    #[Route('/product/{id<\d+>}', name: 'product_show', methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        dd($id);
     }
 }
