6983e96d27eab54e9af8fda5062b5ccf410ca768
Clemens Sietas <siecle@gmx.de>
2026-06-08 22:46:37 +0200

    Adding SubmitType for a submit button and configure options for size

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,11 @@ class ProductType extends AbstractType
         $builder
             ->add('name')
             ->add('description')
-            ->add('size')
+            ->add('size', options: ['attr' => ['min' => '0']])
             ->add('is_available')
+            ->add('save', SubmitType::class, [
+                'label' => 'Add new Product'
+            ]);
         ;
     }
 
