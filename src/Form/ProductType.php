200c4fa629c0e5abdffe5f133eabcd50d3f6cfd9
Clemens Sietas <siecle@gmx.de>
2026-06-09 20:21:24 +0200

    Checkboxes for Categories and repaired path

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,6 +23,13 @@ class ProductType extends AbstractType
                 ]
             ])
             ->add('is_available')
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false
+            ])
             ->add('save', SubmitType::class, ['label' => 'Add New Product'])
         ;
     }
