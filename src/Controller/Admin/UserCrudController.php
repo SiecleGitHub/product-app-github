9854a6c0c41a36806a766dd9e41c940bebee8417
Clemens Sietas <siecle@gmx.de>
2026-06-10 23:15:43 +0200

    Configure the User Admin index and form

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,26 +3,50 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
+        $password_field = TextField::new('plainPassword', 'Password')
+                            ->setFormType(PasswordType::class)
+                            ->onlyOnForms()
+                            ->setRequired(true);
+
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            EmailField::new('email'),
+            BooleanField::new('is_verified'),
+            $password_field
         ];
     }
-    */
+
+    public function persistEntity(EntityManagerInterface $entityManager,
+                                  $entityInstance): void
+    {
+        $entityInstance->setPassword(
+            $this->hasher->hashPassword(
+                $entityInstance,
+                $entityInstance->getPlainPassword()
+            )
+        );
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -39,6 +39,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private bool $isVerified = false;
 
+    private ?string $plain_password = null;
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plain_password;
+    }
+
+    public function setPlainPassword(?string $plain_password): void
+    {
+        $this->plain_password = $plain_password;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
